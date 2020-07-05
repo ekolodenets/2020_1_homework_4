09c6c86955444cee9498c2df3201e73845978983
ekolodenets <66313736+ekolodenets@users.noreply.github.com>
2020-07-05 20:28:16 +0300

    не работает upper функция

--- Tasks/TypingDecorator.py
+++ Tasks/TypingDecorator.py
@@ -1,6 +1,7 @@
 '''
 Декоратор типов
-Напишите декоратор, который проверял бы тип параметров функции следующим образом: При вызове без аргументов осуществлял бы конвертацию параметров и возвращаемого значения в указанные типы:
+Напишите декоратор, который проверял бы тип параметров функции следующим образом: При вызове без аргументов осуществлял
+бы конвертацию параметров и возвращаемого значения в указанные типы:
 
 @typed
 def add(a: int, b: int) -> str:
@@ -30,33 +31,79 @@ acc(0.1, 0.2, 0.4) -> 0.7000000000000001
 
 class MyClass:
 
+    # def typed(n):
+    #     def sub(f):
+    #         def wrapper(self, *args):
+    #             lst = []
+    #             lst2 = []
+    #             for i in args:
+    #                 if i is int(i) or i is float(i):
+    #                     lst.append(i)
+    #                     if len(args) == len(lst):
+    #                         a = sum(args)
+    #                 else:
+    #                     for i in args:
+    #                         lst2.append(str(i))
+    #                     a = ''.join(lst2)
+    #             if n == True and msg == int(msg):
+    #                 msg = "TypeError('`convert_upper` argument `msg` required to be a `str` instance')"
+    #             else:
+    #                 msg = args.upper()
+    #             return a, msg
+    #         return wrapper
+    #     return sub
+
+    def typed(f):
+        def wrapper(self, *args):
+            st = ''
+            n = 0
+            for i in args:
+                st += str(i)
+            if st.isdigit():
+                for i in args:
+                    n += i
+                a = n
+            else:
+                try:
+                    for i in args:
+                        i == float(i)
+                except:
+                    a = st
+                else:
+                    for i in args:
+                        n += float(i)
+                        a = n
+            return a
+        return wrapper
+
     @typed
-    def add(a: int, b: int) -> str:
+    def add(self, a: int, b: int) -> str:
         return a + b
-
-    @typed(strict=True)
-    def convert_upper(msg: str) -> str:
-        return msg.upper()
-
+    #
+    # @typed(strict=True)
+    # def convert_upper(self, msg: str) -> str:
+    #     return msg.upper()
+    #
     @typed
-    def acc(a, b, c):
+    def acc(self, a, b, c):
         return a + b + c
 
 if __name__ == '__main__':
-    # Here we can make console input and check how function works
 
-    a =
-    b =
+    # Here we can make console input and check how function works
+    #
+    a = 'b'
+    b = 0
 
     result = MyClass().add(a, b)
     print(result)
 
-    msg =
-    result = MyClass().convert_upper(msg)
-    print(result)
+    # msg = "abs"
+    # result = MyClass().convert_upper(msg)
+    # print(result)
 
-    a =
-    b =
-    c =
-    result = MyClass().convert_upper(a, b, c)
+    a =3
+    b ='y'
+    c =5
+    result = MyClass().acc(a, b, c)
     print(result)
